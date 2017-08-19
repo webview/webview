8ec0a5c2ddfd5d8d45ca72951a73eaf03590d8e6
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-08-19 14:32:11 +0300

    golang: add minimal cgo wrapper

---
+++ webview.go
@@ -0,0 +1,37 @@
+package webview
+
+/*
+#cgo linux CFLAGS: -DWEBVIEW_GTK=1
+#cgo linux pkg-config: gtk+-3.0 webkitgtk-3.0
+
+#cgo windows CFLAGS: -DWEBVIEW_WINAPI=1
+#cgo windows LDFLAGS: -lole32 -lcomctl32 -loleaut32 -luuid -mwindows
+
+#cgo macos CFLAGS: -DWEBVIEW_COCOA=1 -x objective-c
+#cgo macos LDFLAGS: -framework Cocoa -framework WebKit
+
+#include <stdlib.h>
+#include "webview.h"
+*/
+import "C"
+import (
+	"errors"
+	"unsafe"
+)
+
+func Open(title, url string, w, h int, resizable bool) error {
+	titleStr := C.CString(title)
+	defer C.free(unsafe.Pointer(titleStr))
+	urlStr := C.CString(url)
+	defer C.free(unsafe.Pointer(urlStr))
+	resize := C.int(0)
+	if resizable {
+		resize = C.int(1)
+	}
+
+	r := C.webview(titleStr, urlStr, C.int(w), C.int(h), resize)
+	if r != 0 {
+		return errors.New("failed to create webview")
+	}
+	return nil
+}
