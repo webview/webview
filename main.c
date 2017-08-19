d671232c8d408a1929713d7efd87babd7ee6914a
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-08-19 12:13:58 +0300

    example: add more command line options

--- main.c
+++ main.c
@@ -1,11 +1,40 @@
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "webview.h"
 
+static void usage(char *app) { printf("USAGE: %s <html|url>\n", app); }
+
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    printf("USAGE: %s <html|url>\n", argv[0]);
+  int w = 800;
+  int h = 600;
+  int resize = 0;
+  int opt;
+
+  while ((opt = getopt(argc, argv, "w:h:r")) != -1) {
+    switch (opt) {
+    case 'w':
+      w = atoi(optarg);
+      break;
+    case 'h':
+      h = atoi(optarg);
+      break;
+    case 'r':
+      resize = 1;
+      break;
+    default:
+      usage(argv[0]);
+      return 0;
+    }
+  }
+
+  if (optind != argc - 1) {
+    usage(argv[0]);
     return 1;
   }
-  int r = webview("WebView example", argv[1], 800, 600, 1);
+
+  webview("WebView example", argv[optind], w, h, resize);
   printf("exiting...\n");
-  return r;
+  return 0;
 }
