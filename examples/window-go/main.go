415b1b9a9782f5ab648a64148fe75b7e504edab3
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-09-27 09:08:23 +0300

    examples: add example to demonstrate set_title api

---
+++ examples/window-go/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"strings"
+
+	"github.com/zserge/webview"
+)
+
+const (
+	windowWidth  = 480
+	windowHeight = 320
+)
+
+var indexHTML = `
+<!doctype html>
+<html>
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	</head>
+	<body>
+		<button onclick="external.invoke_('close')">Close</button>
+		<button onclick="external.invoke_('changeTitle:'+document.getElementById('new-title').value)">
+			Change title
+		</button>
+		<input id="new-title" type="text" />
+	</body>
+</html>
+`
+
+func startServer() string {
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		defer ln.Close()
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(indexHTML))
+		})
+		log.Fatal(http.Serve(ln, nil))
+	}()
+	return "http://" + ln.Addr().String()
+}
+
+func handleRPC(w webview.WebView, data string) {
+	switch {
+	case data == "close":
+		w.Terminate()
+	case strings.HasPrefix(data, "changeTitle:"):
+		w.SetTitle(strings.TrimPrefix(data, "changeTitle:"))
+	}
+}
+
+func main() {
+	url := startServer()
+	w := webview.New(webview.Settings{
+		Width:  windowWidth,
+		Height: windowHeight,
+		Title:  "Simple window demo",
+		URL:    url,
+		ExternalInvokeCallback: handleRPC,
+	})
+	defer w.Exit()
+	w.Run()
+}
