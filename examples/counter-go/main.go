b60ad5e8b11f9b65ee4fe5a682ee4b71d44799a6
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-11-02 23:24:35 +0200

    example: fix lint warning

--- examples/counter-go/main.go
+++ examples/counter-go/main.go
@@ -12,12 +12,12 @@ type Counter struct {
 	Value int `json:"value"`
 }
 
-// Add() increases the value of a counter by n
+// Add increases the value of a counter by n
 func (c *Counter) Add(n int) {
 	c.Value = c.Value + int(n)
 }
 
-// Reset() sets the value of a counter back to zero
+// Reset sets the value of a counter back to zero
 func (c *Counter) Reset() {
 	c.Value = 0
 }
