24cb5849bc2a08af885b759ec818e46f675edeab
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-11-02 23:27:57 +0200

    go: split test into smaller sub-tests

--- webview_test.go
+++ webview_test.go
@@ -47,44 +47,51 @@ func TestBindingCall(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	// Call with primitive arguments
-	if !b.Call(`{"scope":"test","method":"Foo1","params":[3,4.5]}`) {
-		t.Fatal()
-	}
-	if foo.Result.(float64) != 7.5 {
-		t.Fatal(foo)
-	}
+	t.Run("Primitives", func(t *testing.T) {
+		if !b.Call(`{"scope":"test","method":"Foo1","params":[3,4.5]}`) {
+			t.Fatal()
+		}
+		if foo.Result.(float64) != 7.5 {
+			t.Fatal(foo)
+		}
+	})
 
-	// Call with slices arrays and maps
-	if !b.Call(`{"scope":"test","method":"Foo2","params":[[1,2,3],[4.5,4.6,4.7],{"1":2,"3":4}]}`) {
-		t.Fatal()
-	}
-	m := foo.Result.(map[string]interface{})
-	if ints := m["a"].([]int); ints[0] != 1 || ints[1] != 2 || ints[2] != 3 {
-		t.Fatal(foo)
-	}
-	if floats := m["b"].([3]float32); floats[0] != 4.5 || floats[1] != 4.6 || floats[2] != 4.7 {
-		t.Fatal(foo)
-	}
-	if dict := m["c"].(map[int]int); len(dict) != 2 || dict[1] != 2 || dict[3] != 4 {
-		t.Fatal(foo)
-	}
-	// Call with structs
-	if !b.Call(`{"scope":"test","method":"Foo3","params":[[{"X":1,"Y":2},{"X":3,"Y":4}],{"Z":42}]}`) {
-		t.Fatal()
-	}
-	m = foo.Result.(map[string]interface{})
-	if p := m["a"].([]image.Point); p[0].X != 1 || p[0].Y != 2 || p[1].X != 3 || p[1].Y != 4 {
-		t.Fatal(foo)
-	}
-	if z := m["b"].(struct{ Z int }); z.Z != 42 {
-		t.Fatal(foo)
-	}
+	t.Run("Collections", func(t *testing.T) {
+		// Call with slices, arrays and maps
+		if !b.Call(`{"scope":"test","method":"Foo2","params":[[1,2,3],[4.5,4.6,4.7],{"1":2,"3":4}]}`) {
+			t.Fatal()
+		}
+		m := foo.Result.(map[string]interface{})
+		if ints := m["a"].([]int); ints[0] != 1 || ints[1] != 2 || ints[2] != 3 {
+			t.Fatal(foo)
+		}
+		if floats := m["b"].([3]float32); floats[0] != 4.5 || floats[1] != 4.6 || floats[2] != 4.7 {
+			t.Fatal(foo)
+		}
+		if dict := m["c"].(map[int]int); len(dict) != 2 || dict[1] != 2 || dict[3] != 4 {
+			t.Fatal(foo)
+		}
+	})
 
-	if b.Call(`{"scope":"foo"}`) || b.Call(`{"scope":"test", "method":"Bar"}`) {
-		t.Fatal()
-	}
-	if b.Call(`{"scope":"test","method":"Foo1","params":["3",4.5]}`) {
-		t.Fatal()
-	}
+	t.Run("Structs", func(t *testing.T) {
+		if !b.Call(`{"scope":"test","method":"Foo3","params":[[{"X":1,"Y":2},{"X":3,"Y":4}],{"Z":42}]}`) {
+			t.Fatal()
+		}
+		m := foo.Result.(map[string]interface{})
+		if p := m["a"].([]image.Point); p[0].X != 1 || p[0].Y != 2 || p[1].X != 3 || p[1].Y != 4 {
+			t.Fatal(foo)
+		}
+		if z := m["b"].(struct{ Z int }); z.Z != 42 {
+			t.Fatal(foo)
+		}
+	})
+
+	t.Run("Errors", func(t *testing.T) {
+		if b.Call(`{"scope":"foo"}`) || b.Call(`{"scope":"test", "method":"Bar"}`) {
+			t.Fatal()
+		}
+		if b.Call(`{"scope":"test","method":"Foo1","params":["3",4.5]}`) {
+			t.Fatal()
+		}
+	})
 }
