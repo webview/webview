294e88537fedb0d684051694fdad5acf7433d691
Sergii Zaitsev <sergii.zaitsev@sixt.com>
2019-02-11 13:57:12 +0100

    more comments

--- webview.h
+++ webview.h
@@ -34,34 +34,77 @@ extern "C" {
 
 typedef void *webview_t;
 
-// Create a new webview instance
-WEBVIEW_API webview_t webview_create(int debug, void *wnd);
+// Creates a new webview instance. If debug is non-zero - developer tools will
+// be enabled (if the platform supports them). Window parameter can be a
+// pointer to the native window handle. If it's non-null - then child WebView
+// is embedded into the given parent window. Otherwise a new window is created.
+// Depending on the platform, a GtkWindow, NSWindow or HWND pointer can be
+// passed here.
+WEBVIEW_API webview_t webview_create(int debug, void *window);
 
-// Destroy a webview
+// Destroys a webview and closes the native window.
 WEBVIEW_API void webview_destroy(webview_t w);
 
-// Run the main loop
+// Runs the main loop until it's terminated. After this function exits - you
+// must destroy the webview.
 WEBVIEW_API void webview_run(webview_t w);
 
-// Stop the main loop
+// Stops the main loop. It is safe to call this function from another other
+// background thread.
 WEBVIEW_API void webview_terminate(webview_t w);
 
-// Post a function to be executed on the main thread
+// Posts a function to be executed on the main thread. You normally do not need
+// to call this function, unless you want to tweak the native window.
 WEBVIEW_API void
 webview_dispatch(webview_t w, void (*fn)(webview_t w, void *arg), void *arg);
 
+// Returns a native window handle pointer. When using GTK backend the pointer
+// is GtkWindow pointer, when using Cocoa backend the pointer is NSWindow
+// pointer, when using Win32 backend the pointer is HWND pointer.
 WEBVIEW_API void *webview_get_window(webview_t w);
 
+// Updates the title of the native window. Must be called from the UI thread.
 WEBVIEW_API void webview_set_title(webview_t w, const char *title);
 
+// Updates native window position and size.
+// TODO: implement x/y and describe possible flags.
 WEBVIEW_API void webview_set_bounds(webview_t w, int x, int y, int width,
                                     int height, int flags);
+
+// Returns native window position and size.
+// TODO: not implemented yet.
 WEBVIEW_API void webview_get_bounds(webview_t w, int *x, int *y, int *width,
                                     int *height, int *flags);
 
+// Navigates webview to the given URL. URL may be a data URI, i.e.
+// "data:text/text,<html>...</html>". It is often ok not to url-encode it
+// properly, webview will re-encode it for you.
 WEBVIEW_API void webview_navigate(webview_t w, const char *url);
+
+// Injects JavaScript code at the initialization of the new page. Every time
+// the webview will open a the new page - this initialization code will be
+// executed. It is guaranteed that code is executed before window.onload.
 WEBVIEW_API void webview_init(webview_t w, const char *js);
+
+// Evaluates arbitrary JavaScript code. Evaluation happens asynchronously, also
+// the result of the expression is ignored. Use RPC bindings if you want to
+// receive notifications about the results of the evaluation.
 WEBVIEW_API void webview_eval(webview_t w, const char *js);
+
+// Binds a native C callback so that it will appear under the given name as a
+// global JavaScript function. Internally it uses webview_init(). Callback
+// receives a request string and a user-provided argument pointer. Request
+// string is a JSON array of all the arguments passed to the JavaScript
+// function.
+WEBVIEW_API void webview_bind(webview_t w, const char *name,
+                              void (*fn)(const char *, void *), void *arg);
+
+// Allows to return a value from the native binding. Original request pointer
+// must be provided to help internal RPC engine match requests with responses.
+// If status is zero - result is expected to be a valid JSON result value.
+// If status is not zero - result is an error JSON object.
+WEBVIEW_API void webview_return(webview_t w, const char *req, int status,
+                                const char *result);
 
 #ifdef __cplusplus
 }
