a06822b8341567c11ac682ce227ea7e4d3bef593
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2019-12-21 20:15:19 +0100

    fix compiler flags on macos

--- script/build.sh
+++ script/build.sh
@@ -5,7 +5,7 @@ set -e
 DIR="$(cd "$(dirname "$0")/../" && pwd)"
 
 if [ "$(uname)" == "Darwin" ]; then
-	FLAGS="-DWEBVIEW_GTK -std=c++11 -Wall -Wextra -pedantic $(pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0)"
+	FLAGS="-DWEBVIEW_COCOA -std=c++11 -Wall -Wextra -pedantic -framework WebKit"
 else
 	FLAGS="-DWEBVIEW_GTK -std=c++11 -Wall -Wextra -pedantic $(pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0)"
 fi
