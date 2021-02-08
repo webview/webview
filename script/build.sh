37ea00f4050ff534c089142ffb48cbf201382b16
Rajiv Shah <rajivshah1@icloud.com>
2021-02-08 15:11:04 -0500

    ci: Build both architectures on macOS

--- script/build.sh
+++ script/build.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+# Support targeting other architectures on macOS
+if [ -z "$MACOS_TARGET_ARCH" ]; then
+    MACOS_TARGET_ARCH="x86_64"
+fi
+
 DIR="$(cd "$(dirname "$0")/../" && pwd)"
 
 if [ "$(uname)" = "Darwin" ]; then
-	FLAGS="-DWEBVIEW_COCOA -std=c++11 -Wall -Wextra -pedantic -framework WebKit"
+	FLAGS="-DWEBVIEW_COCOA -std=c++11 -Wall -Wextra -pedantic -framework WebKit -arch $MACOS_TARGET_ARCH"
 else
 	FLAGS="-DWEBVIEW_GTK -std=c++11 -Wall -Wextra -pedantic $(pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0)"
 fi
@@ -34,12 +39,16 @@ c++ main.cc $FLAGS -o webview
 echo "Building test app"
 c++ webview_test.cc $FLAGS -o webview_test
 
-echo "Running tests"
-./webview_test
-
-if command -v go >/dev/null 2>&1 ; then
-	echo "Running Go tests"
-	CGO_ENABLED=1 go test
+if [ "$SKIP_TESTS" = true ]; then
+	echo "Skipping tests"
 else
-	echo "SKIP: Go tests"
+	echo "Running tests"
+	./webview_test
+
+	if command -v go >/dev/null 2>&1 ; then
+		echo "Running Go tests"
+		CGO_ENABLED=1 go test
+	else
+		echo "SKIP: Go tests"
+	fi
 fi
