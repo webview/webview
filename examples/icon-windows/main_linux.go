72e3634c4009029ed528524e75829c81fb39bfd6
Leo <leo.steiner@mesheven.com>
2022-03-18 18:42:43 +1100

    Removed hardcoded CFLAGS/LDFLAGS

--- examples/icon-windows/main_linux.go
+++ examples/icon-windows/main_linux.go
@@ -3,8 +3,7 @@
 package main
 
 /*
-#cgo CFLAGS: -pthread -I/usr/include/gtk-3.0 -I/usr/include/at-spi2-atk/2.0 -I/usr/include/at-spi-2.0 -I/usr/include/dbus-1.0 -I/usr/lib/x86_64-linux-gnu/dbus-1.0/include -I/usr/include/gtk-3.0 -I/usr/include/gio-unix-2.0/ -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/harfbuzz -I/usr/include/pango-1.0 -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pixman-1 -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/libpng16 -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include
-#cgo LDFLAGS:  -lgtk-3 -lgdk-3 -lpangocairo-1.0 -lpango-1.0 -latk-1.0 -lcairo-gobject -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0
+#cgo linux pkg-config: gtk+-3.0
 
 #include <gtk/gtk.h>
 
