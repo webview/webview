962ebb311cf412a8444ac3e834e7c00f38e88701
dandeto <danielmichaelmills@gmail.com>
2022-04-03 17:07:17 -0500

    Remove dlls.bat

--- script/build.bat
+++ script/build.bat
@@ -18,16 +18,6 @@ if not exist "%script_dir%\microsoft.web.webview2.%nuget_version%" (
 	echo Nuget package installed
 )
 
-if not exist "%src_dir%\dll\x64\webview.dll" (
-	call dlls.bat
-)
-if not exist "%build_dir%\webview.dll" (
-	copy "%src_dir%\dll\x64\webview.dll" %build_dir%
-)
-if not exist "%build_dir%\WebView2Loader.dll" (
-	copy "%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\x64\WebView2Loader.dll" "%build_dir%"
-)
-
 echo Looking for vswhere.exe...
 set "vswhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 if not exist "%vswhere%" set "vswhere=%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe"
@@ -46,6 +36,37 @@ if not exist "%vc_dir%\Common7\Tools\vsdevcmd.bat" (
 	exit /b 1
 )
 echo Found %vc_dir%
+
+:: build dlls if not found
+if not exist "%src_dir%\dll\x64\webview.dll" (
+	mkdir "%src_dir%\dll\x86"
+	mkdir "%src_dir%\dll\x64"
+	copy  "%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\x64\WebView2Loader.dll" "%src_dir%\dll\x64"
+	copy  "%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\x86\WebView2Loader.dll" "%src_dir%\dll\x86"
+
+	call "%vc_dir%\Common7\Tools\vsdevcmd.bat" -arch=x86 -host_arch=x64
+
+	echo "Building webview.dll (x86)"
+	cl /D "WEBVIEW_API=__declspec(dllexport)" ^
+		/I "%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\include" ^
+		"%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\x86\WebView2Loader.dll.lib" ^
+		/std:c++17 /EHsc "/Fo%build_dir%"\ ^
+		"%src_dir%\webview.cc" /link /DLL "/OUT:%src_dir%\dll\x86\webview.dll" || exit \b
+
+	call "%vc_dir%\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64
+	echo "Building webview.dll (x64)"
+	cl /D "WEBVIEW_API=__declspec(dllexport)" ^
+		/I "%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\include" ^
+		"%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\x64\WebView2Loader.dll.lib" ^
+		/std:c++17 /EHsc "/Fo%build_dir%"\ ^
+		"%src_dir%\webview.cc" /link /DLL "/OUT:%src_dir%\dll\x64\webview.dll" || exit \b
+)
+if not exist "%build_dir%\webview.dll" (
+	copy "%src_dir%\dll\x64\webview.dll" %build_dir%
+)
+if not exist "%build_dir%\WebView2Loader.dll" (
+	copy "%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\x64\WebView2Loader.dll" "%build_dir%"
+)
 
 call "%vc_dir%\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64
 
