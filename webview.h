e8ffd3682584c49776ac44eb534e9b454a44d9e3
Josias <me@josias.dev>
2022-05-19 12:24:05 -0700

    Navigate to about:blank rather than predefined blank page (#747)

--- webview.h
+++ webview.h
@@ -1089,8 +1089,7 @@ public:
 
   void navigate(const std::string &url) {
     if (url == "") {
-      browser_engine::navigate("data:text/html," +
-                               url_encode("<html><body></body></html>"));
+      browser_engine::navigate("about:blank");
       return;
     }
     browser_engine::navigate(url);
