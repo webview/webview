fa1396df2b7f675d7c059dc68c05af68866a62f6
Steffen André Langnes <steffenl.sw@gmail.com>
2022-05-27 16:38:10 +0900

    Add test for narrow/wide string conversion

--- webview_test.cc
+++ webview_test.cc
@@ -4,6 +4,7 @@
 #include "webview.h"
 
 #include <cassert>
+#include <cstdint>
 #include <cstring>
 #include <iostream>
 #include <thread>
@@ -166,13 +167,57 @@ static void test_percent_encode() {
   assert(percent_encode("æøå") == "%C3%A6%C3%B8%C3%A5");
 }
 
+#if _WIN32
+// =================================================================
+// TEST: ensure that narrow/wide string conversion works on Windows.
+// =================================================================
+static void test_win32_narrow_wide_string_conversion() {
+  using namespace webview::detail;
+  assert(widen_string("") == L"");
+  assert(narrow_string(L"") == "");
+  assert(widen_string("foo") == L"foo");
+  assert(narrow_string(L"foo") == "foo");
+  assert(widen_string("フー") == L"フー");
+  assert(narrow_string(L"フー") == "フー");
+  assert(widen_string("æøå") == L"æøå");
+  assert(narrow_string(L"æøå") == "æøå");
+  // Unicode number for the smiley face below: U+1F600
+  assert(widen_string("😀") == L"😀");
+  assert(narrow_string(L"😀") == "😀");
+  // Ensure that elements of wide string are correct
+  {
+    auto s = widen_string("😀");
+    assert(s.size() == 2);
+    assert(static_cast<std::uint16_t>(s[0]) == 0xD83D);
+    assert(static_cast<std::uint16_t>(s[1]) == 0xDE00);
+  }
+  // Ensure that elements of narrow string are correct
+  {
+    auto s = narrow_string(L"😀");
+    assert(s.size() == 4);
+    assert(static_cast<std::uint8_t>(s[0]) == 0xf0);
+    assert(static_cast<std::uint8_t>(s[1]) == 0x9f);
+    assert(static_cast<std::uint8_t>(s[2]) == 0x98);
+    assert(static_cast<std::uint8_t>(s[3]) == 0x80);
+  }
+  // Null-characters must also be converted
+  assert(widen_string(std::string(2, '\0')) == std::wstring(2, L'\0'));
+  assert(narrow_string(std::wstring(2, L'\0')) == std::string(2, '\0'));
+}
+#endif
+
 int main(int argc, char *argv[]) {
   std::unordered_map<std::string, std::function<void()>> all_tests = {
-      {"terminate", test_terminate},
-      {"c_api", test_c_api},
-      {"bidir_comms", test_bidir_comms},
-      {"json", test_json},
-      {"percent_encode", test_percent_encode}};
+    {"terminate", test_terminate},
+    {"c_api", test_c_api},
+    {"bidir_comms", test_bidir_comms},
+    {"json", test_json},
+    {"percent_encode", test_percent_encode},
+#if _WIN32
+    {"win32_narrow_wide_string_conversion",
+     test_win32_narrow_wide_string_conversion},
+#endif
+  };
   // Without arguments run all tests, one-by-one by forking itself.
   // With a single argument - run the requested test
   if (argc == 1) {
