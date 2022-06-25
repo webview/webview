148d4dc488a038a1862c56328d63f0ae23e88a40
Steffen André Langnes <steffenl.sw@gmail.com>
2022-06-25 23:56:11 +0900

    Use static C/C++ runtime library with cgo on Windows

--- webview.go
+++ webview.go
@@ -8,7 +8,7 @@ package webview
 #cgo darwin LDFLAGS: -framework WebKit
 
 #cgo windows CXXFLAGS: -DWEBVIEW_EDGE -std=c++17
-#cgo windows LDFLAGS: -lWebView2Loader -lole32 -lshell32 -lshlwapi -luser32
+#cgo windows LDFLAGS: -static-libstdc++ -static-libgcc -lWebView2Loader -lole32 -lshell32 -lshlwapi -luser32
 
 #include "webview.h"
 
