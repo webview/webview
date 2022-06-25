aeaca4cb523604f9c169260975a59fcff702dea7
Steffen André Langnes <steffenl.sw@gmail.com>
2022-06-26 00:57:56 +0900

    Use CGO_CXXFLAGS instead of CGO_CPPFLAGS in Windows build script

--- script/build.bat
+++ script/build.bat
@@ -122,7 +122,7 @@ cl %warning_params% ^
 
 echo Building Go examples
 mkdir build\examples\go
-set CGO_CPPFLAGS="-I%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\include"
+set CGO_CXXFLAGS="-I%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\include"
 set CGO_LDFLAGS="-L%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\x64"
 go build -ldflags="-H windowsgui" -o build\examples\go\basic.exe examples\basic.go || exit /b
 go build -ldflags="-H windowsgui" -o build\examples\go\bind.exe examples\bind.go || exit /b
