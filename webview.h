0b22f0128bc5a792638e815c0420c09ecb22881c
Steffen André Langnes <steffenl.sw@gmail.com>
2022-06-27 00:01:17 +0900

    Add myself to copyright notice

--- webview.h
+++ webview.h
@@ -2,6 +2,7 @@
  * MIT License
  *
  * Copyright (c) 2017 Serge Zaitsev
+ * Copyright (c) 2022 Steffen André Langnes
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
