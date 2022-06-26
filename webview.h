37eaac3a82542829eb22ea9e397a6748680e4913
Steffen André Langnes <steffenl.sw@gmail.com>
2022-06-26 17:00:14 +0900

    Move registry code into class
    Cleans up the code and frees resources properly at runtime.

--- webview.h
+++ webview.h
@@ -982,6 +982,67 @@ struct shcore_symbols {
       library_symbol<SetProcessDpiAwareness_t>("SetProcessDpiAwareness");
 };
 
+class reg_key {
+public:
+  explicit reg_key(HKEY root_key, const wchar_t *sub_key, DWORD options,
+                   REGSAM sam_desired) {
+    HKEY handle;
+    auto status =
+        RegOpenKeyExW(root_key, sub_key, options, sam_desired, &handle);
+    if (status == ERROR_SUCCESS) {
+      m_handle = handle;
+    }
+  }
+
+  explicit reg_key(HKEY root_key, const std::wstring &sub_key, DWORD options,
+                   REGSAM sam_desired)
+      : reg_key(root_key, sub_key.c_str(), options, sam_desired) {}
+
+  virtual ~reg_key() {
+    if (m_handle) {
+      RegCloseKey(m_handle);
+      m_handle = nullptr;
+    }
+  }
+
+  reg_key(const reg_key &other) = delete;
+  reg_key &operator=(const reg_key &other) = delete;
+  reg_key(reg_key &&other) = delete;
+  reg_key &operator=(reg_key &&other) = delete;
+
+  bool is_open() const { return !!m_handle; }
+  bool get_handle() const { return m_handle; }
+
+  std::wstring query_string(const wchar_t *name) const {
+    DWORD buf_length = 0;
+    // Get the size of the data in bytes.
+    auto status = RegQueryValueExW(m_handle, name, nullptr, nullptr, nullptr,
+                                   &buf_length);
+    if (status != ERROR_SUCCESS && status != ERROR_MORE_DATA) {
+      return std::wstring();
+    }
+    // Read the data.
+    std::wstring result(buf_length / sizeof(wchar_t), 0);
+    auto buf = reinterpret_cast<LPBYTE>(&result[0]);
+    status =
+        RegQueryValueExW(m_handle, name, nullptr, nullptr, buf, &buf_length);
+    if (status != ERROR_SUCCESS) {
+      return std::wstring();
+    }
+    // Remove trailing null-characters.
+    for (std::size_t length = result.size(); length > 0; --length) {
+      if (result[length - 1] != 0) {
+        result.resize(length);
+        break;
+      }
+    }
+    return result;
+  }
+
+private:
+  HKEY m_handle = nullptr;
+};
+
 bool enable_dpi_awareness() {
   auto user32 = native_library(L"user32.dll");
   if (auto fn = user32.get(user32_symbols::SetProcessDpiAwarenessContext)) {
@@ -1031,34 +1092,11 @@ std::wstring find_edge_webview_client_dll() {
   std::wstring stable_release_guid = L"{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}";
   std::wstring sub_key =
       L"SOFTWARE\\Microsoft\\EdgeUpdate\\ClientState\\" + stable_release_guid;
-  HKEY root_key = HKEY_LOCAL_MACHINE;
-  REGSAM sam = KEY_READ | KEY_WOW64_32KEY;
-  HKEY key;
-  LSTATUS status = RegOpenKeyExW(root_key, sub_key.c_str(), 0, sam, &key);
-  if (status != ERROR_SUCCESS) {
+  reg_key key(HKEY_LOCAL_MACHINE, sub_key, 0, KEY_READ | KEY_WOW64_32KEY);
+  if (!key.is_open()) {
     return std::wstring();
   }
-  auto value_name = L"EBWebView";
-  DWORD buf_length = 0;
-  status =
-      RegQueryValueExW(key, value_name, nullptr, nullptr, nullptr, &buf_length);
-  if (status != ERROR_SUCCESS && status != ERROR_MORE_DATA) {
-    return std::wstring();
-  }
-  std::wstring client_dll_path(buf_length / sizeof(wchar_t), 0);
-  auto buf = reinterpret_cast<LPBYTE>(&client_dll_path[0]);
-  status =
-      RegQueryValueExW(key, value_name, nullptr, nullptr, buf, &buf_length);
-  if (status != ERROR_SUCCESS) {
-    return std::wstring();
-  }
-  for (std::size_t length = client_dll_path.size(); length > 0; --length) {
-    if (client_dll_path[length - 1] != 0) {
-      client_dll_path.resize(length);
-      break;
-    }
-  }
-  RegCloseKey(key);
+  auto client_dll_path = key.query_string(L"EBWebView");
   client_dll_path += L"\\EBWebView\\";
 #if defined(_M_X64) || defined(_M_AMD64)
   client_dll_path += L"x64";
