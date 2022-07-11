f3e363c791f6d318346a94ea2dab1fe43f69980f
Steffen André Langnes <steffenl.sw@gmail.com>
2022-07-11 18:01:04 +0900

    Check _WIN32 instead of WIN32
    With MSVC, _WIN32 is always defined while WIN32 is only defined after
    including the Windows.h header. Since the C examples did not include the
    Windows header then WIN32 was not defined and therefore the executable
    did not use the proper WinMain entry point.

--- examples/basic.c
+++ examples/basic.c
@@ -1,7 +1,11 @@
 #include "webview.h"
 #include <stddef.h>
 
-#ifdef WIN32
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+#ifdef _WIN32
 int WINAPI WinMain(HINSTANCE hInt, HINSTANCE hPrevInst, LPSTR lpCmdLine,
                    int nCmdShow) {
 #else
