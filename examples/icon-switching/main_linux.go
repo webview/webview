3b81983a4106a562cea1d9b1fbf25aa879b8c743
ldstein <ldstein78@gmail.com>
2022-11-30 10:15:16 +1100

    Added icon switching demo for Windows & Linux (#687)
    * Added icon switching demo for Windows
    
    * Added linux demo
    
    * Added build instructions for Windows
    
    * Removed hardcoded CFLAGS/LDFLAGS
    
    * Fixed bad filename
    
    * Renamed example icon-windows to icon-switching
    
    * Changed first icon to IDI_APPLICATION in icon switching example
    
    * Changed hwnd to gtk_window for clarity in icon-switching demo

---
+++ examples/icon-switching/main_linux.go
@@ -0,0 +1,53 @@
+// This example shows you how to change the icon in the webview on Linux
+
+package main
+
+/*
+#cgo linux pkg-config: gtk+-3.0
+
+#include <gtk/gtk.h>
+
+void set_external_icon(void *ptr, char* iconPath) {
+	gtk_window_set_icon_from_file(ptr, iconPath, NULL);
+}
+*/
+import "C"
+import (
+	"time"
+	"unsafe"
+
+	"github.com/webview/webview"
+)
+
+// Set icon from external file
+func setExternalIcon(w webview.WebView, iconPath string) {
+
+	gtk_window := w.Window()
+	cstr := C.CString(iconPath)
+
+	defer C.free(unsafe.Pointer(cstr))
+
+	C.set_external_icon(gtk_window, cstr)
+}
+
+func main() {
+	w := webview.New(false)
+
+	setExternalIcon(w, "icons/png/fish.png")
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		setExternalIcon(w, "icons/png/tiger.png")
+		time.Sleep(3 * time.Second)
+		setExternalIcon(w, "icons/png/zebra.png")
+		time.Sleep(3 * time.Second)
+		setExternalIcon(w, "icons/png/peacock.png")
+	}()
+
+	defer w.Destroy()
+	w.SetTitle("Icon example")
+	w.SetSize(800, 600, webview.HintNone)
+	w.Navigate("https://duckduckgo.com")
+
+	w.Run()
+}
---
+++ examples/icon-switching/main_windows.go
@@ -0,0 +1,86 @@
+// This example shows you how to change the icon in the webview on Windows
+//
+// To build:
+// - Install Go-Winres: go install github.com/tc-hib/go-winres@latest
+// - Generate syso files: go-winres make
+// - Compile exe: go build .
+
+package main
+
+/*
+#include <windows.h>
+
+void set_external_icon(const void *ptr, char* iconPath) {
+
+	HICON iconBig = LoadImage(NULL, iconPath, IMAGE_ICON, GetSystemMetrics(SM_CXICON  ), GetSystemMetrics(SM_CXICON  ), LR_LOADFROMFILE);
+	HICON iconSml = LoadImage(NULL, iconPath, IMAGE_ICON, GetSystemMetrics(SM_CXSMICON), GetSystemMetrics(SM_CXSMICON), LR_LOADFROMFILE);
+
+	if (iconSml) SendMessage((HWND)ptr, WM_SETICON, ICON_SMALL, (LPARAM)iconSml);
+	if (iconBig) SendMessage((HWND)ptr, WM_SETICON, ICON_BIG  , (LPARAM)iconBig);
+}
+
+void set_resource_icon(const void *ptr, char* name) {
+
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+
+	HICON iconBig = (HICON)LoadImage(hInstance, name, IMAGE_ICON, GetSystemMetrics(SM_CXICON  ), GetSystemMetrics(SM_CXICON  ), LR_DEFAULTCOLOR);
+	HICON iconSml = (HICON)LoadImage(hInstance, name, IMAGE_ICON, GetSystemMetrics(SM_CXSMICON), GetSystemMetrics(SM_CYSMICON), LR_DEFAULTCOLOR);
+
+
+	if (iconSml) SendMessage((HWND)ptr, WM_SETICON, ICON_SMALL, (LPARAM)iconSml);
+	if (iconBig) SendMessage((HWND)ptr, WM_SETICON, ICON_BIG  , (LPARAM)iconBig);
+}
+
+*/
+import "C"
+import (
+	"time"
+	"unsafe"
+
+	"github.com/webview/webview"
+)
+
+// Set icon from external file
+func setExternalIcon(w webview.WebView, iconPath string) {
+
+	hwnd := w.Window()
+	cstr := C.CString(iconPath)
+
+	defer C.free(unsafe.Pointer(cstr))
+
+	C.set_external_icon(hwnd, cstr)
+}
+
+// Set icon from resources embedded in rsrc_windows_amd64.syso / rsrc_windows_386.syso
+// In this example, syso files were generated using https://github.com/tc-hib/go-winres
+func setResourceIcon(w webview.WebView, resourceName string) {
+
+	hwnd := w.Window()
+	cstr := C.CString(resourceName)
+
+	defer C.free(unsafe.Pointer(cstr))
+
+	C.set_resource_icon(hwnd, cstr)
+}
+
+func main() {
+	w := webview.New(false)
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		setResourceIcon(w, "TIGER")
+		time.Sleep(3 * time.Second)
+		setExternalIcon(w, "icons/ico/zebra.ico")
+		time.Sleep(3 * time.Second)
+		setExternalIcon(w, "icons/ico/peacock.ico")
+		time.Sleep(3 * time.Second)
+		setResourceIcon(w, "#32512") // #32512 = IDI_APPLICATION
+	}()
+
+	defer w.Destroy()
+	w.SetTitle("Icon example")
+	w.SetSize(800, 600, webview.HintNone)
+	w.Navigate("https://duckduckgo.com")
+
+	w.Run()
+}
