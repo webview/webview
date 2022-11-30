aedee9285f77ae832014b72b232c1d32e97d0c73
Steffen André Langnes <steffenl.sw@gmail.com>
2022-12-01 04:57:22 +0900

    Add tests for bind/unbind

--- webview_test.cc
+++ webview_test.cc
@@ -49,6 +49,120 @@ static void test_c_api() {
 }
 
 // =================================================================
+// TEST: use C API to test binding and unbinding.
+// =================================================================
+
+static void test_c_api_bind() {
+  struct context_t {
+    webview_t w;
+    unsigned int number;
+  } context{};
+  auto test = +[](const char *seq, const char *req, void *arg) {
+    auto increment = +[](const char *seq, const char *req, void *arg) {
+      ++static_cast<context_t *>(arg)->number;
+    };
+    auto context = static_cast<context_t *>(arg);
+    std::string req_(req);
+    // Bind and increment number.
+    if (req_ == "[0]") {
+      assert(context->number == 0);
+      webview_bind(context->w, "increment", increment, context);
+      webview_return(
+          context->w, seq, 0,
+          "(() => {try{window.increment()}catch{}window.test(1)})()");
+      return;
+    }
+    // Unbind and make sure that we cannot increment even if we try.
+    if (req_ == "[1]") {
+      assert(context->number == 1);
+      webview_unbind(context->w, "increment");
+      webview_return(
+          context->w, seq, 0,
+          "(() => {try{window.increment()}catch{}window.test(2)})()");
+      return;
+    }
+    // Number should not have changed but we can bind again and change the number.
+    if (req_ == "[2]") {
+      assert(context->number == 1);
+      webview_bind(context->w, "increment", increment, context);
+      webview_return(
+          context->w, seq, 0,
+          "(() => {try{window.increment()}catch{}window.test(3)})()");
+      return;
+    }
+    // Finish test.
+    if (req_ == "[3]") {
+      assert(context->number == 2);
+      webview_terminate(context->w);
+      return;
+    }
+    assert(!"Should not reach here");
+  };
+  auto html = "<script>\n"
+              "  window.test(0);\n"
+              "</script>";
+  auto w = webview_create(false, nullptr);
+  context.w = w;
+  // Attempting to remove non-existing binding is OK
+  webview_unbind(w, "test");
+  webview_bind(w, "test", test, &context);
+  // Attempting to bind multiple times only binds once
+  webview_bind(w, "test", test, &context);
+  webview_set_html(w, html);
+  webview_run(w);
+}
+
+// =================================================================
+// TEST: test synchronous binding and unbinding.
+// =================================================================
+
+static void test_sync_bind() {
+  unsigned int number = 0;
+  webview::webview w(false, nullptr);
+  auto test = [&](const std::string &req) -> std::string {
+    auto increment = [&](const std::string &req) -> std::string {
+      ++number;
+      return "";
+    };
+    // Bind and increment number.
+    if (req == "[0]") {
+      assert(number == 0);
+      w.bind("increment", increment);
+      return "(() => {try{window.increment()}catch{}window.test(1)})()";
+    }
+    // Unbind and make sure that we cannot increment even if we try.
+    if (req == "[1]") {
+      assert(number == 1);
+      w.unbind("increment");
+      return "(() => {try{window.increment()}catch{}window.test(2)})()";
+    }
+    // Number should not have changed but we can bind again and change the number.
+    if (req == "[2]") {
+      assert(number == 1);
+      w.bind("increment", increment);
+      return "(() => {try{window.increment()}catch{}window.test(3)})()";
+    }
+    // Finish test.
+    if (req == "[3]") {
+      assert(number == 2);
+      w.terminate();
+      return "";
+    }
+    assert(!"Should not reach here");
+  };
+  auto html = "<script>\n"
+              "  window.test(0);\n"
+              "</script>";
+  // Attempting to remove non-existing binding is OK
+  w.unbind("test");
+  w.bind("test", test);
+  // Attempting to bind multiple times only binds once
+  w.bind("test", test);
+  w.set_html(html);
+  w.run();
+}
+
+// =================================================================
 // TEST: webview_version().
 // =================================================================
 static void test_c_api_version() {
@@ -198,11 +312,10 @@ static void test_win32_narrow_wide_string_conversion() {
 
 int main(int argc, char *argv[]) {
   std::unordered_map<std::string, std::function<void()>> all_tests = {
-      {"terminate", test_terminate},
-      {"c_api", test_c_api},
-      {"c_api_version", test_c_api_version},
-      {"bidir_comms", test_bidir_comms},
-      {"json", test_json}};
+      {"terminate", test_terminate},     {"c_api", test_c_api},
+      {"c_api_bind", test_c_api_bind},   {"c_api_version", test_c_api_version},
+      {"bidir_comms", test_bidir_comms}, {"json", test_json},
+      {"sync_bind", test_sync_bind}};
 #if _WIN32
   all_tests.emplace("win32_narrow_wide_string_conversion",
                     test_win32_narrow_wide_string_conversion);
