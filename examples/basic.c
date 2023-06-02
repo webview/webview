11fb7eeb59751fe9e70cbe7afe2ab36c67456956
Tao Zuhong <taozuhong@gmail.com>
2023-06-02 10:47:45 +0800

    Add MINGW script to build example

--- examples/basic.c
+++ examples/basic.c
@@ -14,7 +14,7 @@ int main() {
   webview_t w = webview_create(0, NULL);
   webview_set_title(w, "Basic Example");
   webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
-  webview_set_html(w, "Thanks for using webview!");
+  webview_set_html(w, "<html><head><title>Basic Example</title></head><body><div style='text-align:center; width:100%; height:50px'>Thanks for using webview!</div><div><iframe src='https://www.datatable.online/' width='100%' height='100%'/></div></body></html>");
   webview_run(w);
   webview_destroy(w);
   return 0;
