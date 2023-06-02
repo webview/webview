ea1c8603e1d99727dc944a96e09fd91d0e42c61b
Tao Zuhong <taozuhong@gmail.com>
2023-06-02 21:26:02 +0800

    Update with review suggestions

--- script/mingw.sh
+++ script/mingw.sh
@@ -10,7 +10,7 @@ if [ ! -d "${DIR_ROOT}/build" ]; then
 fi
 
 # If you update the nuget package, change its version here
-nuget_version="1.0.1150.38"
+nuget_version="1.0.1774.30"
 echo "${DIR_ROOT}/build/Microsoft.Web.WebView2.${nuget_version}"
 echo -n "Check and install Nuget Package Microsoft.Web.WebView2.${nuget_version}......"
 if [ ! -d "${DIR_ROOT}/build/Microsoft.Web.WebView2.${nuget_version}" ]; then
@@ -18,42 +18,22 @@ if [ ! -d "${DIR_ROOT}/build/Microsoft.Web.WebView2.${nuget_version}" ]; then
 	${DIR_ROOT}/build/nuget.exe install Microsoft.Web.Webview2 -Version ${nuget_version} -OutputDirectory "${DIR_ROOT}/build" -Verbosity quiet
 	echo "[Installed]"
 else
-    echo "[Installed]"
+  echo "[Installed]"
 fi
 
 echo -n "Build webview object......"
 g++ -c webview.cc -std=c++17 -Ibuild/Microsoft.Web.WebView2.${nuget_version}/build/native/include -o build/webview.o
-if [ $? -eq 0 ]; then
-  echo "[done]"
-else
-  echo "[failed]"
-fi
+echo "[done]"
 
 echo -n "Build webview library......"
 g++ -shared -D "WEBVIEW_API=__declspec(dllexport)" build/webview.o -mwindows -ladvapi32 -lole32 -lshell32 -lshlwapi -luser32 -lversion -o build/webview.dll
-if [ $? -eq 0 ]; then
-  echo "[done]"
-else
-  echo "[failed]"
-fi
+echo "[done]"
 
 echo -n "Build webview example(link with dll)......"
 gcc -std=c99 -I. -L./build -mwindows -lwebview -o build/basic_dll.exe examples/basic.c
-if [ $? -eq 0 ]; then
-  echo "[done]"
-else
-  echo "[failed]"
-fi
+echo "[done]"
 
 echo -n "Build webview example(stand-alone)......"
 gcc -c examples/basic.c -std=c99 -I. -o build/basic.o
 g++ build/basic.o build/webview.o -mwindows -ladvapi32 -lole32 -lshell32 -lshlwapi -luser32 -lversion -o build/basic_std.exe
-if [ $? -eq 0 ]; then
-  echo "[done]"
-else
-  echo "[failed]"
-fi
-
-echo "Run webview example......"
-build/basic_dll.exe &
-build/basic_std.exe &
+echo "[done]"
