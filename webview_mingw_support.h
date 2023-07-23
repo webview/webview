69263f40544a923ab60bfa7030a6652b39a7167d
Steffen André Langnes <steffenl.sw@gmail.com>
2023-07-24 02:01:18 +0900

    Adjustments to MinGW support header

--- webview_mingw_support.h
+++ webview_mingw_support.h
@@ -2,12 +2,19 @@
 #define WEBVIEW_MINGW_SUPPORT_H
 #ifdef _WIN32
 
-#ifndef __eventtoken_h__
+// Some MinGW distributions do not have the EventToken.h header (used by WebView2).
+// Define the things used by WebView2 from the EventToken.h header if needed.
+#if defined(__has_include)
+#if !defined(WEBVIEW_HAVE_EVENTTOKEN_H) && __has_include(<EventToken.h>)
+#define WEBVIEW_HAVE_EVENTTOKEN_H
+#endif
+#if !defined(WEBVIEW_HAVE_EVENTTOKEN_H) && defined(__eventtoken_h__)
+#define WEBVIEW_HAVE_EVENTTOKEN_H
+#endif
+#if !defined(WEBVIEW_HAVE_EVENTTOKEN_H)
 #include <cstdint>
-
-typedef struct EventRegistrationToken {
-  int64_t value;
-} EventRegistrationToken;
+typedef struct EventRegistrationToken { int64_t value; } EventRegistrationToken;
+#endif
 #endif
 
 #endif /* _WIN32 */
