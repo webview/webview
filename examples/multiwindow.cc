9460377f0592262f21d8b08497df2ed71a5cbfe9
Steffen André Langnes <steffenl.sw@gmail.com>
2023-08-24 19:03:42 +0900

    Add some code for Windows in example

--- examples/multiwindow.cc
+++ examples/multiwindow.cc
@@ -18,7 +18,7 @@ constexpr const auto primary_html =
 constexpr const auto secondary_html = R"html(<div>Secondary window</div>)html";
 
 #ifdef _WIN32
-int WINAPI WinMain(HINSTANCE /*hInst*/, HINSTANCE /*hPrevInst*/,
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE /*hPrevInst*/,
                    LPSTR /*lpCmdLine*/, int /*nCmdShow*/) {
 #else
 int main() {
@@ -30,12 +30,36 @@ int main() {
   primary.set_size(480, 320, WEBVIEW_HINT_NONE);
 
   primary.bind("newWindow", [&](const std::string & /*req*/) -> std::string {
-#ifdef WEBVIEW_GTK
-    auto *native_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-#else
+#if defined(__APPLE__)
 #error Not implemented yet
+#elif defined(__unix__)
+    auto *native_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+#elif defined(_WIN32)
+      WNDCLASSEXW wc{};
+      wc.cbSize = sizeof(WNDCLASSEX);
+      wc.hInstance = hInst;
+      wc.lpszClassName = L"example_window";
+      wc.lpfnWndProc = +[](HWND hwnd, UINT msg, WPARAM wp,
+                                     LPARAM lp) -> LRESULT {
+        switch (msg) {
+        case WM_SIZE:
+          // TODO: Resize widget
+          break;
+        case WM_CLOSE:
+          DestroyWindow(hwnd);
+          break;
+        default:
+          return DefWindowProcW(hwnd, msg, wp, lp);
+        }
+        return 0;
+      };
+      RegisterClassExW(&wc);
+      auto *native_window = CreateWindowW(L"example_window", L"", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+                    CW_USEDEFAULT, 0, 0, nullptr, nullptr, hInst, nullptr);
+#else
+#error Unsupported platform
 #endif
-    auto &secondary = *instances.emplace(instances.end(), false, native_window);
+    auto &secondary = *instances.emplace(instances.end(), false, &native_window);
     secondary.set_title("Secondary Window");
     secondary.set_size(480, 320, WEBVIEW_HINT_NONE);
     secondary.set_html(secondary_html);
