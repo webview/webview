45cd7200a491ca8f14dbf0302f6ec2ee6aa5a1f9
fantaisie-software <58218392+fantaisie-software@users.noreply.github.com>
2023-11-29 11:57:00 +0100

    Reduce interference with user-owned window (Cocoa) (#1049)
    This also allows WebView to be put in a regular NSView (Cocoa) instead of forcing NSWindow.
    
    ---------
    
    Co-authored-by: Fred Laboureur <fred@purebasic.com>
    Co-authored-by: Steffen André Langnes <steffenl.sw@gmail.com>

--- webview.h
+++ webview.h
@@ -1182,7 +1182,10 @@ private:
       };
       )"");
     objc::msg_send<void>(m_window, "setContentView:"_sel, m_webview);
-    objc::msg_send<void>(m_window, "makeKeyAndOrderFront:"_sel, nullptr);
+
+    if (m_owns_window) {
+      objc::msg_send<void>(m_window, "makeKeyAndOrderFront:"_sel, nullptr);
+    }
   }
   int on_application_should_terminate(id /*delegate*/, id app) {
     dispatch([app, this] {
