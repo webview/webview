8665d57beba9003540b6deb88e468d254334fbe6
Steffen André Langnes <steffenl.sw@gmail.com>
2024-01-06 23:59:08 +0900

    Revert "Check for X11 using GDK"
    This reverts commit e5e24bbcb25119fe03384ddf8e56d351a7808c21.

--- webview.h
+++ webview.h
@@ -752,10 +752,6 @@ inline std::string json_parse(const std::string &s, const std::string &key,
 #include <gtk/gtk.h>
 #include <webkit2/webkit2.h>
 
-#ifdef GDK_WINDOWING_X11
-#include <gdk/gdkx.h>
-#endif
-
 #include <fcntl.h>
 #include <sys/stat.h>
 
@@ -811,24 +807,11 @@ static inline bool is_using_nvidia_driver() {
   return S_ISDIR(buffer.st_mode);
 }
 
-// Checks whether the default display is using the GDK X11 backend.
-// See: https://docs.gtk.org/gdk3/class.DisplayManager.html
-static inline bool is_x11_display() {
-#ifdef GDK_WINDOWING_X11
-  auto *manager = gdk_display_manager_get();
-  auto *display = gdk_display_manager_get_default_display(manager);
-  return GDK_IS_X11_DISPLAY(display);
-#else
-  return false;
-#endif
-}
-
 // Checks whether WebKit is affected by bug when using DMA-BUF renderer.
 // Returns true if all of the following conditions are met:
 //  - WebKit version is >= 2.42 (please narrow this down when there's a fix).
 //  - Environment variables are empty or not set:
 //    - WEBKIT_DISABLE_DMABUF_RENDERER
-//  - Windowing system is X11.
 //  - NVIDIA GPU driver is used.
 static inline bool is_webkit_dmabuf_bugged() {
   auto wk_major = webkit_get_major_version();
@@ -839,9 +822,6 @@ static inline bool is_webkit_dmabuf_bugged() {
     return false;
   }
   if (!get_env("WEBKIT_DISABLE_DMABUF_RENDERER").empty()) {
-    return false;
-  }
-  if (!is_x11_display()) {
     return false;
   }
   if (!is_using_nvidia_driver()) {
