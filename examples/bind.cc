8b62093f4e8621ea789d116a4046e77c6c5d3e91
Steffen André Langnes <steffenl.sw@gmail.com>
2024-02-26 19:24:48 +0900

    Rework error handling and reporting
    C/C++ API:
    * Add "webview_error_t" enum with error codes.
    
    C++ API:
    * Add "error_info" class to hold error information (code, message).
    * Add "result" and "noresult" classes for returning to the caller either
      a success value or error information. "result" should be used for
      functions that return a value; "noresult" should be used for functions
      that don't return a value. Either case allows caller to check whether
      the call was successful, and if not, check the error information.
      The return type can be constructed from the value or "error_info"
      instance. For example, returning "{}" can construct a successful
      "noresult", "{...}" can construct a successful `result<...>` and
      "error_info{...}" can construct a result with an error.
    * Add "exception" class that can be thrown in constructors.
    * Constructors may now throw "exception".
    * Add try-catch block to C++ examples because constructors may throw.
    
    C API:
    * Functions should return type "webview_error_t" to allow for error
      handling. "api_filter()" catches exceptions and converts error
      information into "webview_error_t".
    * Function arguments are now checked to reduce chance of crashing.

--- examples/bind.cc
+++ examples/bind.cc
@@ -1,6 +1,7 @@
 #include "webview.h"
 
 #include <chrono>
+#include <iostream>
 #include <string>
 #include <thread>
 
@@ -43,36 +44,41 @@ int WINAPI WinMain(HINSTANCE /*hInst*/, HINSTANCE /*hPrevInst*/,
 #else
 int main() {
 #endif
-  long count = 0;
+  try {
+    long count = 0;
 
-  webview::webview w(true, nullptr);
-  w.set_title("Bind Example");
-  w.set_size(480, 320, WEBVIEW_HINT_NONE);
+    webview::webview w(true, nullptr);
+    w.set_title("Bind Example");
+    w.set_size(480, 320, WEBVIEW_HINT_NONE);
 
-  // A binding that counts up or down and immediately returns the new value.
-  w.bind("count", [&](const std::string &req) -> std::string {
-    // Imagine that req is properly parsed or use your own JSON parser.
-    auto direction = std::stol(req.substr(1, req.size() - 1));
-    return std::to_string(count += direction);
-  });
+    // A binding that counts up or down and immediately returns the new value.
+    w.bind("count", [&](const std::string &req) -> std::string {
+      // Imagine that req is properly parsed or use your own JSON parser.
+      auto direction = std::stol(req.substr(1, req.size() - 1));
+      return std::to_string(count += direction);
+    });
 
-  // A binding that creates a new thread and returns the result at a later time.
-  w.bind(
-      "compute",
-      [&](const std::string &id, const std::string &req, void * /*arg*/) {
-        // Create a thread and forget about it for the sake of simplicity.
-        std::thread([&, id, req] {
-          // Simulate load.
-          std::this_thread::sleep_for(std::chrono::seconds(1));
-          // Imagine that req is properly parsed or use your own JSON parser.
-          const auto *result = "42";
-          w.resolve(id, 0, result);
-        }).detach();
-      },
-      nullptr);
+    // A binding that creates a new thread and returns the result at a later time.
+    w.bind(
+        "compute",
+        [&](const std::string &id, const std::string &req, void * /*arg*/) {
+          // Create a thread and forget about it for the sake of simplicity.
+          std::thread([&, id, req] {
+            // Simulate load.
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            // Imagine that req is properly parsed or use your own JSON parser.
+            const auto *result = "42";
+            w.resolve(id, 0, result);
+          }).detach();
+        },
+        nullptr);
 
-  w.set_html(html);
-  w.run();
+    w.set_html(html);
+    w.run();
+  } catch (const webview::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
 
   return 0;
 }
