78bd2b8d25a6c3ba87ff53f477361d014f91b624
Steffen André Langnes <steffenl.sw@gmail.com>
2024-08-21 15:11:47 +0900

    Document root webview.h

--- core/include/webview.h
+++ core/include/webview.h
@@ -1,3 +1,12 @@
+/**
+ * @file webview.h
+ *
+ * @deprecated This header file is deprecated. Use `webview/core/webview.h` instead.
+ *
+ * This file is provided for backward-compatibility with existing code
+ * such as `#include "webview.h"`.
+ */
+
 #ifndef WEBVIEW_ROOT_H
 #define WEBVIEW_ROOT_H
 
