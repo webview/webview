d512842ec3db67cc445efb61aaacfab141c6e6f7
Steffen André Langnes <steffenl.sw@gmail.com>
2024-09-16 22:37:11 +0900

    Reformat Python script

--- scripts/amalgamate.py
+++ scripts/amalgamate.py
@@ -3,14 +3,16 @@ from dataclasses import dataclass
 import os
 import re
 
+
 @dataclass
 class ProcessorContext:
     include_pattern = re.compile(r'#include "([^"]+)"\n')
-    block_comment_pattern = re.compile(r'^/\*.*?\*/\n', re.DOTALL)
+    block_comment_pattern = re.compile(r"^/\*.*?\*/\n", re.DOTALL)
     visited_files = set()
     ordered_files = []
     chunks = []
     visited_copyright_notices = set()
+
 
 def process_file(context: ProcessorContext, input: os.PathLike):
     input = os.path.realpath(input)
@@ -27,11 +29,13 @@ def process_file(context: ProcessorContext, input: os.PathLike):
     with open(input, encoding="utf-8") as f:
         content = f.read()
 
-        content = context.block_comment_pattern.sub(lambda m: replace_copyright(context, m), content)
+        content = context.block_comment_pattern.sub(
+            lambda m: replace_copyright(context, m), content
+        )
 
         end = 0
         for m in context.include_pattern.finditer(content):
-            context.chunks.append(content[end:m.start(0)])
+            context.chunks.append(content[end : m.start(0)])
             end = m.end(0)
             include_file = os.path.join(os.path.dirname(input), m[1])
 
@@ -47,6 +51,7 @@ def process_file(context: ProcessorContext, input: os.PathLike):
             process_file(context, include_file)
         context.chunks.append(content[end:])
 
+
 def replace_copyright(context: ProcessorContext, match: re.Match[str]):
     if not "copyright".casefold() in match[0].casefold():
         return match[0]
@@ -56,6 +61,7 @@ def replace_copyright(context: ProcessorContext, match: re.Match[str]):
     context.visited_copyright_notices.add(hashed)
     return match[0]
 
+
 def main(options):
     context = ProcessorContext()
     for input in options.input:
@@ -64,12 +70,16 @@ def main(options):
         for chunk in context.chunks:
             f.write(chunk)
 
+
 def parse_args():
-    parser = ArgumentParser(prog="amalgamate",
-                                     description="Combines a C++ source/header hierarchy into one file")
+    parser = ArgumentParser(
+        prog="amalgamate",
+        description="Combines a C or C++ source/header hierarchy into one file",
+    )
     parser.add_argument("input", nargs="+", help="Input file")
     parser.add_argument("output", help="Output file")
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     main(parse_args())
