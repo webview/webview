6b831f0c732ed03deacb6fd56c1670961e6a9b60
citkane <michael@openpoint.ie>
2025-02-01 18:21:34 +0000

    Add preinstall for nuget

--- examples/node.napi/dist/windows.preinstall.js
+++ examples/node.napi/dist/windows.preinstall.js
@@ -1,11 +1,14 @@
 "use strict";
-const fs = require("node:fs");
-const path = require("node:path");
-const { execSync } = require("node:childProcess");
-const dest = path.join(process.cwd(), "nuget.exe");
-console.log(dest);
-if (!fs.existsSync(dest)) {
-    const url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
-    const command = `wget -O ${dest} ${url}`;
-    execSync(command, { stdio: "inherit" });
+const os = require("node:os");
+if (os.platform() === "win32") {
+    const fs = require("node:fs");
+    const path = require("node:path");
+    const { execSync } = require("node:child_process");
+    const dest = path.join(process.cwd(), "nuget.exe");
+    console.log(dest);
+    if (!fs.existsSync(dest)) {
+        const url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
+        const command = `wget -O ${dest} ${url}`;
+        execSync(command, { stdio: "inherit" });
+    }
 }
--- examples/node.napi/src/windows.preinstall.ts
+++ examples/node.napi/src/windows.preinstall.ts
@@ -1,13 +1,15 @@
-const fs = require("node:fs");
-const path = require("node:path");
-const { execSync } = require("node:childProcess");
-const dest = path.join(process.cwd(), "nuget.exe");
+const os = require("node:os");
+if (os.platform() === "win32") {
+  const fs = require("node:fs");
+  const path = require("node:path");
+  const { execSync } = require("node:child_process");
+  const dest = path.join(process.cwd(), "nuget.exe");
+  console.log(dest);
 
-console.log(dest);
+  if (!fs.existsSync(dest)) {
+    const url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
 
-if (!fs.existsSync(dest)) {
-  const url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
-
-  const command = `wget -O ${dest} ${url}`;
-  execSync(command, { stdio: "inherit" });
+    const command = `wget -O ${dest} ${url}`;
+    execSync(command, { stdio: "inherit" });
+  }
 }
