f8262b70fdf1202d632fb64290ccc40bb4fa3368
citkane <michael@openpoint.ie>
2025-02-01 18:12:12 +0000

    Add preinstall for nuget

---
+++ examples/node.napi/dist/windows.preinstall.js
@@ -0,0 +1,11 @@
+"use strict";
+const fs = require("node:fs");
+const path = require("node:path");
+const { execSync } = require("node:childProcess");
+const dest = path.join(process.cwd(), "nuget.exe");
+console.log(dest);
+if (!fs.existsSync(dest)) {
+    const url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
+    const command = `wget -O ${dest} ${url}`;
+    execSync(command, { stdio: "inherit" });
+}
