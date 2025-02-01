63e663b3aca1c8e9af109f906c2ce7d2cdcaa863
citkane <michael@openpoint.ie>
2025-02-01 19:18:13 +0000

    Add preinstall for nuget

--- examples/node.napi/dist/windows.preinstall.js
+++ examples/node.napi/dist/windows.preinstall.js
@@ -1,13 +1,54 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const os = require("node:os");
+const fs = require("node:fs");
+const path = require("node:path");
+const { exec } = require("node:child_process");
+const nugetDest = path.join(process.cwd(), "nuget.exe");
+const MsWebView2Dir = path.join(process.cwd(), "src");
+const nugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
 if (os.platform() === "win32") {
-    const fs = require("node:fs");
-    const path = require("node:path");
-    const { execSync } = require("node:child_process");
-    const dest = path.join(process.cwd(), "src", "nuget.exe");
-    console.log(dest);
-    if (!fs.existsSync(dest)) {
-        const url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
-        execSync("wGet", ["-O", dest, url], { stdio: "inherit" });
-    }
+    fetchNuget()
+        .then(downloadMsWebView2)
+        .catch((err) => {
+        throw err;
+    });
+}
+function fetchNuget() {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (fs.existsSync(nugetDest))
+            return;
+        try {
+            const res = yield fetch(nugetUrl);
+            if (!res.ok)
+                throw new Error(`HTTP error downloading nuget.exe! status: ${res.status}`);
+            const buffer = yield res.arrayBuffer();
+            fs.writeFileSync(nugetDest, Buffer.from(buffer), { mode: 0o755 });
+        }
+        catch (err) {
+            throw err;
+        }
+    });
+}
+function downloadMsWebView2() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const command = `nuget.exe install Microsoft.Web.WebView2 ${MsWebView2Dir}`;
+        try {
+            const cp = yield exec(command, (err) => {
+                if (err)
+                    throw err;
+            });
+        }
+        catch (err) {
+            throw err;
+        }
+    });
 }
--- examples/node.napi/src/windows.preinstall.ts
+++ examples/node.napi/src/windows.preinstall.ts
@@ -1,14 +1,40 @@
 const os = require("node:os");
+const fs = require("node:fs");
+const path = require("node:path");
+const { exec } = require("node:child_process");
+const nugetDest = path.join(process.cwd(), "nuget.exe");
+const MsWebView2Dir = path.join(process.cwd(), "src");
+const nugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
+
 if (os.platform() === "win32") {
-  const fs = require("node:fs");
-  const path = require("node:path");
-  const { execSync } = require("node:child_process");
-  const dest = path.join(process.cwd(), "src", "nuget.exe");
-  console.log(dest);
+  fetchNuget()
+    .then(downloadMsWebView2)
+    .catch((err) => {
+      throw err;
+    });
+}
 
-  if (!fs.existsSync(dest)) {
-    const url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
-
-    execSync("wGet", ["-O", dest, url], { stdio: "inherit" });
+async function fetchNuget() {
+  if (fs.existsSync(nugetDest)) return;
+  try {
+    const res = await fetch(nugetUrl);
+    if (!res.ok)
+      throw new Error(
+        `HTTP error downloading nuget.exe! status: ${res.status}`
+      );
+    const buffer = await res.arrayBuffer();
+    fs.writeFileSync(nugetDest, Buffer.from(buffer), { mode: 0o755 });
+  } catch (err) {
+    throw err;
+  }
+}
+async function downloadMsWebView2() {
+  const command = `nuget.exe install Microsoft.Web.WebView2 ${MsWebView2Dir}`;
+  try {
+    const cp = await exec(command, (err: Error) => {
+      if (err) throw err;
+    });
+  } catch (err) {
+    throw err;
   }
 }
